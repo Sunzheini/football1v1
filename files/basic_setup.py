55eeadf2793c8f36969213b0c23cd01e41f50da6
Sunzheini <daniel_zorov@abv.bg>
2022-11-28 11:54:36 +0200

    update2_28112022

--- files/basic_setup.py
+++ files/basic_setup.py
@@ -1,3 +1,4 @@
+import os
 from abc import ABC, abstractmethod
 import pygame
 pygame.font.init()   # enable the fonts
@@ -7,22 +8,67 @@ pygame.mixer.init()  # enable sounds
 class BasicSetup(ABC):
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 500
-    WINDOW_COLOR = (97, 97, 97)
+
+    GREY_COLOR = (97, 97, 97)
+    BLUE_COLOR = (0, 0, 255)
+    GREEN_COLOR = (200, 255, 0)
 
     FPS = 60
+
+    SCORE_FONT = pygame.font.SysFont('arial', 30)
+    WINNER_FONT = pygame.font.SysFont('arial', 60)
+
+    GOAL_SOUND = pygame.mixer.Sound(os.path.join('.', 'assets', 'goal.mp3'))
+    GOAL_SOUND.set_volume(0.5)  # lower volume
+    HIT_BALL_SOUND = pygame.mixer.Sound(os.path.join('.', 'assets', 'hit_ball.mp3'))
+    GOAL_SOUND.set_volume(0.3)  # lower volume
+    INITIATE_AFTER_GOAL_SOUND = pygame.mixer.Sound(os.path.join('.', 'assets', 'initiate_after_goal.mp3'))
+    WIN_SOUND = pygame.mixer.Sound(os.path.join('.', 'assets', 'win_sound.mp3'))
+
+    MOVEMENT_VELOCITY = 5
+    BULLET_VELOCITY = 8
+
+    PIECE_WIDTH = 75
+    PIECE_HEIGHT = 75
+    GREEN_PIECE_IMAGE = pygame.image.load(os.path.join('.', 'assets', 'blue.png'))
+    BLUE_PIECE_IMAGE = pygame.image.load(os.path.join('.', 'assets', 'blue.png'))
+
+    BACKGROUND = pygame.image.load(os.path.join('.', 'assets', 'background.jpg'))
+
+    PLAYER_1_KEYS = [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s]  # left, right, up, down
+    PLAYER_2_KEYS = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
+    BORDER = pygame.Rect(WINDOW_WIDTH // 2 - 5, 0, 10, WINDOW_HEIGHT)  # only int
 
     def __init__(self):
         self.window = pygame.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('Football 1v1')
 
+        # transform images before display
+        self.green_piece = pygame.transform.rotate(
+            pygame.transform.scale(self.GREEN_PIECE_IMAGE,
+                                   (self.PIECE_WIDTH, self.PIECE_HEIGHT)), 0)
+        self.blue_piece = pygame.transform.rotate(
+            pygame.transform.scale(self.BLUE_PIECE_IMAGE,
+                                   (self.PIECE_WIDTH, self.PIECE_HEIGHT)), 0)
+        self.background = pygame.transform.scale(self.BACKGROUND,
+                                                 (self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
+
+        # circles for pieces in order to use the rectangles for movement later
+        self.green = pygame.Rect(100, 200, self.PIECE_WIDTH, self.PIECE_HEIGHT)  # x, y, width, height
+        self.purple = pygame.Rect(350, 200, self.PIECE_WIDTH, self.PIECE_HEIGHT)  # x, y, width, height
+
+        # score
+        self.green_score = 0
+        self.blue_score = 0
+
     @staticmethod
     def update_after_any_change():
         pygame.display.update()
 
+    @abstractmethod
     def draw_window(self):
-        self.window.fill(self.WINDOW_COLOR)  # RGB
-        self.update_after_any_change()
+        pass
 
     @abstractmethod
     def run_game(self):
--- main.py
+++ main.py
@@ -5,6 +5,22 @@ pygame.mixer.init()  # enable sounds
 
 
 class NewGame(BasicSetup):
+
+    def draw_window(self):
+        # self.window.fill(self.GREY_COLOR)  # RGB
+        self.window.blit(self.background, (0, 0))
+        pygame.draw.rect(self.window, self.GREY_COLOR, self.BORDER)  # draw the border
+
+        green_player_score_text = self.SCORE_FONT.render(str(self.green_score), True, self.GREEN_COLOR)
+        self.window.blit(green_player_score_text, (10, 10))
+        blue_player_score_text = self.SCORE_FONT.render(str(self.blue_score), True, self.BLUE_COLOR)
+        self.window.blit(blue_player_score_text, (self.WINDOW_WIDTH - blue_player_score_text.get_width()-10, 10))
+
+        self.window.blit(self.green_piece, (50, 50))  # always draw after filling the screen
+        self.window.blit(self.blue_piece, (100, 100))
+
+        self.update_after_any_change()
+
     def run_game(self):
         run = True
 
